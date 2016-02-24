5ecb944f735864f6c21aea05807af27111cf00e1
warren vosper <straydogsw@gmail.com>
2016-02-23 18:42:28 -0600

    wip

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,5 +1,18 @@
 
 describe('LearnJS', function() {
+  it('invokes the router when loaded', function() {
+    spyOn(learnjs, 'showView');
+    learnjs.appOnReady();
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+
+  it('subscribes to the hash change event', function() {
+    learnjs.appOnReady();
+    spyOn(learnjs, 'showView');
+    $(window).trigger('hashchange');
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+
   it('can show a problem view', function() {
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
