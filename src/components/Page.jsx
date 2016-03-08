33bce79859fbd8c456f67774113983b1be697b1c
warren vosper <straydogsw@gmail.com>
2016-03-08 11:48:22 -0700

    wip

---
+++ src/components/Page.jsx
@@ -0,0 +1,25 @@
+
+
+import React from 'react';
+
+import Header from './Header';
+import Body from './Body';
+import Footer from './Footer';
+
+const Page = (props) => (
+  <div id="home_page">
+    <Header />
+    <Body />
+    <Footer />
+  </div>
+)
+
+Page.propTypes = {
+  //  currentSelectionPanel: React.PropTypes.string.isRequired,
+  //change:                React.PropTypes.func.isRequired,
+  //  toggleBedsSelection:   React.PropTypes.func.isRequired,
+}
+
+Page.displayName = 'Page'
+
+export default Page
---
+++ src/home.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Page from './components/Page';
+
+ReactDOM.render(
+  <Page />,
+  document.getElementById('home_page_container')
+);
